d5d2797ac863d70557de2f6492e9736902fc9242
sajjadisr <sajjadi.sr313@gmail.com>
2026-07-23 10:50:42 +0330

    Fixed issues

--- ai.py
+++ ai.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 from config import GEMINI_API_KEY
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 def generate_content(prompt):
